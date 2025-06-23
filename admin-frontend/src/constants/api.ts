e41d16de04750863e593c23a647268274043e8ca
Faiullin <osmanfajzulla@gmail.com>
2025-06-23 18:32:36 +0500

    Saves

--- admin-frontend/src/constants/api.ts
+++ admin-frontend/src/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_BASE_URL =  process.env.NEXT_PUBLIC_API_BASE_URL;
 export const CODE_RUNNER_API_URL = process.env.NEXT_PUBLIC_CODE_RUNNER_API_URL;
 
 console.log("Constants loaded:", {
